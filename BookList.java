713de259b9ee0a39934d6d097cf747a3c22cca35
Raspbwolf <lucasvarella.rw@hotmail.com>
2017-08-13 18:07:13 -0300

    Update BookList.java

--- BookList.java
+++ BookList.java
@@ -1,9 +1,16 @@
 package exercise2;
 
-public class bookList {
+public class BookList {
 	
 	String[] books;
 	int bookNumber = 0;
+	private BookCtrl owner;
+	
+	public BookList(BookCtrl owner){
+		this.owner = owner;
+		
+	}
+
 
 	public void addBook(String bookName) {
 		bookNumber++;
