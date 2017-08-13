0721f10028e2624cdc993943b04e2a1d9649bae9
Raspbwolf <lucasvarella.rw@hotmail.com>
2017-08-13 17:22:57 -0300

    Create MainScreen.java

---
+++ MainScreen.java
@@ -0,0 +1,34 @@
+package exercise2;
+import java.util.Scanner;
+
+public class MainScreen {
+	
+	private Scanner keyboard;
+	private MainController owner;
+	
+	public MainScreen(MainController owner) {
+		this.owner = owner;
+		keyboard = new Scanner(System.in);
+		
+	}
+
+	public void showMainMenu() {
+		
+		System.out.println("----- SysBooks -----");
+		System.out.println("--- Options:");
+		System.out.println("1. Books");
+		System.out.println("2. Friends");
+		System.out.println("3. Borrowings");
+		System.out.println("4. Borrow History");
+
+		int option = 0;
+		
+		System.out.println("Please input desired Option (1-4) ::");
+		option = keyboard.nextInt();
+		if (option == 1) {
+			owner.showBookMenu();
+		}
+		
+	}
+
+}
