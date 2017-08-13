09b258887a9a6113be1bef636ba932352d8f30d8
Raspbwolf <lucasvarella.rw@hotmail.com>
2017-08-13 18:08:44 -0300

    Update BookCtrl.java

--- BookCtrl.java
+++ BookCtrl.java
@@ -4,18 +4,21 @@ public class BookCtrl {
 	
 	private BookScreen bookScreen;
 	private MainController owner;
+	private BookList bookList;
+
 
 	
 	public BookCtrl(MainController owner) {
 		this.owner = owner;
 		bookScreen = new BookScreen(this);
+		bookList = new BookList(this);
 	}
 
 	public void showBookMenu() {
 		bookScreen.showBookMenu();
 	}
 	public void addBook(String bookName){
-		
+		bookList.addBook(bookName);
 	}
 	
 	
@@ -25,4 +28,3 @@ public class BookCtrl {
 	
 
 }
-
