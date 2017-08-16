6660799a96c7cfb15230792d6aafe73d2362dbfc
Raspbwolf <lucasvarella.rw@hotmail.com>
2017-08-16 18:12:02 -0300

    Update BookScreen.java

--- BookScreen.java
+++ BookScreen.java
@@ -23,30 +23,18 @@ public class BookScreen {
 		System.out.println("2. Alter Book");
 		System.out.println("3. Book List");
 		System.out.println("4. Del. Book");
-		System.out.println("5. Main Menu");
+		System.out.println("5. Borrow Book");
+		System.out.println("6. Return Book");
+		System.out.println("7. Main Menu");
 
-		
 		
 		int option = 0;
 		
 		
-		System.out.println("Please input desired Option (1-4) ::");
+		System.out.println("Please input desired Option (1-7) ::");
 		option = keyboard.nextInt();
 		if (option == 1) {
-			System.out.println("Please input book's name.");
-			keyboard.nextLine();
-			String bookName = keyboard.nextLine();
-			owner.addBook(bookName);
-			System.out.println("Book '" + bookName + "' added!");
-			int choice = 0;
-			System.out.println("Input 1 to go back, or 0 to exit.");
-			choice = keyboard.nextInt();
-			if(choice == 1) {
-				owner.showBookMenu();
-			}else if(choice == 0) {
-				System.out.println("Goodbye!");
-			}
-			
+			owner.addBook();
 		}	
 		if (option == 3) {
 			owner.showBookList();
@@ -59,20 +47,20 @@ public class BookScreen {
 				System.out.println("Goodbye!");
 			}
 		}	
-		if (option == 5) {
+		if (option == 7) {
 			owner.showMainMenu();
 		}	
 		if (option == 4) {
 			System.out.println("Please input book's name for deletion.");
 			keyboard.nextLine();
-			String bookName = keyboard.nextLine();
+			String bookTitle = keyboard.nextLine();
 			String choice = "X";
 			System.out.println("Are you sure you want to delete this book?");
 			System.out.println("Y / N");
 			choice = keyboard.next();
 			if(choice.equals('Y') || choice.equals("y")) {
-				owner.deleteBook(bookName);
-				System.out.println("Book '" + bookName + "' deleted from database!");
+				owner.deleteBook(bookTitle);
+				System.out.println("Book '" + bookTitle + "' deleted from database!");
 				int choice1 = 0;
 				System.out.println("Input 1 to go back, or 0 to exit.");
 				choice1 = keyboard.nextInt();
