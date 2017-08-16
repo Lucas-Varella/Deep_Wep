b507846e586122be1c5fb711d1140a0ba3f457cf
Raspbwolf <lucasvarella.rw@hotmail.com>
2017-08-16 18:08:25 -0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,20 +1,58 @@
-
 package exercise2;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Exercise2 {
+public class Book {
 	
-	public static void main(String[] args) {
-		MainController mainCtrl = new MainController();
-		mainCtrl.start();
-		
-		Friend friend = new Friend();
-		
-		friend.setName("Gerso");
-		friend.setPhone(6969);
-		
-		ArrayList<Friend> friends = new ArrayList<Friend>();
+	private Scanner keyboard;
+	
+	private BookList owner;
+	private String title;
+	private String author;
+	private String numPages;
+	private String bookAbstract;
+	private int ageGroup;
+	private int bookNumber;
+	
+	public Book(BookList owner) {
+		this.owner = owner;
+		keyboard = new Scanner(System.in);
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	public void getDetails(){
+		System.out.println("Name: " + this.title);
+		System.out.println("Author: " + this.author);
+		System.out.println("Number of Pages: " + this.numPages);
+		System.out.println("Book's age group: " + this.ageGroup);
+		System.out.println("Book's Abstract:");
+		System.out.print(this.bookAbstract);
+		int option = 0;
+		System.out.println();
+				
+
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setNumPages(String numPages) {
+		this.numPages = numPages;
+	}
+	public void setBookAbstract(String bookAbstract) {
+		this.bookAbstract = bookAbstract;
+	}
+	public void setAgeGroup(int ageGroup){
+		this.ageGroup = ageGroup;
+	}
+
+	public void setBookNumber(int bookNumber) {
+		this.bookNumber = bookNumber;		
 	}
 
 }
