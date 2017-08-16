3a12acf71d8a665557bf9db8fe9755f5c403508e
Raspbwolf <lucasvarella.rw@hotmail.com>
2017-08-16 18:12:23 -0300

    Update Exercise2.java

--- Exercise2.java
+++ Exercise2.java
@@ -1,12 +1,20 @@
 
 package exercise2;
 
+import java.util.ArrayList;
 
 public class Exercise2 {
 	
 	public static void main(String[] args) {
 		MainController mainCtrl = new MainController();
 		mainCtrl.start();
+		
+		Friend friend = new Friend();
+		
+		friend.setName("Gerso");
+		friend.setPhone(6969);
+		
+		ArrayList<Friend> friends = new ArrayList<Friend>();
 	}
 
 }
