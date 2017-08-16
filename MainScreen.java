26750c213f0dc9f7cce0a390e48ebee38c21fb80
Raspbwolf <lucasvarella.rw@hotmail.com>
2017-08-16 18:15:16 -0300

    Update MainScreen.java

--- MainScreen.java
+++ MainScreen.java
@@ -20,16 +20,20 @@ public class MainScreen {
 		System.out.println("2. Friends");
 		System.out.println("3. Borrowings");
 		System.out.println("4. Borrow History");
+		System.out.println("5. Exit");
 
 		int option = 0;
 		
-		System.out.println("Please input desired Option (1-4) ::");
+		System.out.println("Please input desired Option (1-5) ::");
 		option = keyboard.nextInt();
 		if (option == 1) {
 			owner.showBookMenu();
 		} else if (option == 2) {
 			owner.showFriendMenu();
+		}else if(option == 5) {
+			System.out.println("Goodbye!");
 		}
+		
 		
 	}
 
